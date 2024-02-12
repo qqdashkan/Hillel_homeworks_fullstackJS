ca1c2b825427e0aff86d638f27ef8ea33f38553f
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-02-12 09:43:07 +0200

    NaNami + arrays done

---
+++ script.js
@@ -0,0 +1,34 @@
+let ami = 'ami';
+
+console.log(Number('@') + ami);
+
+let myObject = {
+    name: 'Daria',
+    nickname: 'qqdashkan',
+    phoneNumber: '0673299116',
+    email: 'qqdashkan@icloud.com',
+    gender: 'female',
+    hobby: 'box'
+};
+
+let myObject1 = {
+    name: 'Inna',
+    nickname: 'innatria',
+    phoneNumber: '0671164004',
+    email: 'inna@icloud.com',
+    gender: 'female',
+    hobby: 'travel'
+};
+
+let myObject2 = {
+    name: 'Pablo',
+    nickname: 'Escobar',
+    phoneNumber: '+57864235991',
+    email: 'pablo@icloud.com',
+    gender: 'male',
+    hobby: 'coke'
+};
+
+const myArray = [myObject, myObject1, myObject2];
+
+console.log(myArray);
