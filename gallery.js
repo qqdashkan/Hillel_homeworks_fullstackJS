e86f012fbd09697dda57d9a6a132d2ad09cfa5de
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-04-16 18:41:37 +0300

    gallery created, to do event

---
+++ gallery.js
@@ -0,0 +1,60 @@
+const images = [{
+    src: 'https://dorsiafinance.co.uk/wp-content/uploads/10-Facts-About-Mercedes-Benz.webp',
+    alt: "car"
+  },{
+    src: 'https://media.istockphoto.com/id/1361394182/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D0%B1%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BE%D1%80%D0%BE%D1%82%D0%BA%D0%BE%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%BA%D0%BE%D1%88%D0%BA%D0%B8-%D0%B2%D1%8B%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F%D1%89%D0%B8%D0%B9-%D1%88%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC-%D0%B8%D0%BB%D0%B8-%D1%83%D0%B4%D0%B8%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC.jpg?s=612x612&w=0&k=20&c=yEEyxvdyb-jUxnHmr8nXSf9qQPS0WfkhBITLVIaj7OY=',
+    alt: "cat"
+  }, {
+    src: 'https://th-thumbnailer.cdn-si-edu.com/2JOIjX7EMPggkEfnvggGYxocK9o=/fit-in/1600x0/filters:focal(400x267:401x268)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/b0/94/b094a830-dd4e-4aca-8884-5ab2b8a3625b/gettyimages-152621111_resize.jpg',
+    alt: "cat"
+  },{
+    src: 'https://d128mjo55rz53e.cloudfront.net/media/images/blog-breed-corgi_1_1.max-500x500.format-jpeg.jpg',
+    alt: "dog"
+  }, {
+    src: 'https://avatars.dzeninfra.ru/get-zen_doc/3719229/pub_5f72f6b98eec670acab179e9_5f72f94aff51c40043be4103/smart_quality_scale_1200',
+    alt: "zeus"
+  },{
+    src: 'https://th-thumbnailer.cdn-si-edu.com/2JOIjX7EMPggkEfnvggGYxocK9o=/fit-in/1600x0/filters:focal(400x267:401x268)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/b0/94/b094a830-dd4e-4aca-8884-5ab2b8a3625b/gettyimages-152621111_resize.jpg',
+    alt: "car"
+  }, {
+    src: 'https://dorsiafinance.co.uk/wp-content/uploads/10-Facts-About-Mercedes-Benz.webp',
+    alt: "car"
+  },{
+    src: 'https://d128mjo55rz53e.cloudfront.net/media/images/blog-breed-corgi_1_1.max-500x500.format-jpeg.jpg',
+    alt: "car"
+  }, {
+    src: 'https://media.istockphoto.com/id/1361394182/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D0%B1%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BE%D1%80%D0%BE%D1%82%D0%BA%D0%BE%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%BA%D0%BE%D1%88%D0%BA%D0%B8-%D0%B2%D1%8B%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F%D1%89%D0%B8%D0%B9-%D1%88%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC-%D0%B8%D0%BB%D0%B8-%D1%83%D0%B4%D0%B8%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC.jpg?s=612x612&w=0&k=20&c=yEEyxvdyb-jUxnHmr8nXSf9qQPS0WfkhBITLVIaj7OY=',
+    alt: "car"
+  },{
+    src: 'https://th-thumbnailer.cdn-si-edu.com/2JOIjX7EMPggkEfnvggGYxocK9o=/fit-in/1600x0/filters:focal(400x267:401x268)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/b0/94/b094a830-dd4e-4aca-8884-5ab2b8a3625b/gettyimages-152621111_resize.jpg',
+    alt: "car"
+  }, {
+    src: 'https://avatars.dzeninfra.ru/get-zen_doc/3719229/pub_5f72f6b98eec670acab179e9_5f72f94aff51c40043be4103/smart_quality_scale_1200',
+    alt: "car"
+  },{
+    src: 'https://th-thumbnailer.cdn-si-edu.com/2JOIjX7EMPggkEfnvggGYxocK9o=/fit-in/1600x0/filters:focal(400x267:401x268)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/b0/94/b094a830-dd4e-4aca-8884-5ab2b8a3625b/gettyimages-152621111_resize.jpg',
+    alt: "car"
+  }];
+
+
+  const h1 = document.querySelector('.h1');
+  const gallery = document.createElement('div');
+  h1.after(gallery);
+  gallery.classList.add('wrapper');
+
+    images.forEach(img => {
+        const pic = document.createElement('img');
+        pic.style.margin = '15px';
+        pic.classList.add('picture');
+        pic.setAttribute('src', img.src);
+        pic.setAttribute('alt', img.alt);
+        pic.setAttribute('width', '400px');
+        gallery.appendChild(pic);
+    });
+
+gallery.addEventListener('click', function (event) {
+    console.log(event.target);
+})
+
+
+
