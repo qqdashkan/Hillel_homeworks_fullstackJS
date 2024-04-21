7bd6e45983891663f21bdb150c91ab7482f5a87e
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-04-21 16:38:03 +0300

    design sircles

--- script.js
+++ script.js
@@ -1,26 +1,98 @@
 const canvas = document.querySelector('.space');
 const colors = document.querySelector('.colors');
 
-canvas.addEventListener('dblclick', function (event) {
+canvas.addEventListener('dblclick', function () {
     const newAvailableColor = document.createElement('div');
     colors.appendChild(newAvailableColor);
     newAvailableColor.classList.add('availableColor');
 
-    const newCircleElement = document.createElement('div');
-    this.appendChild(newCircleElement);
-    newCircleElement.classList.add('circle');
+    const red = Math.random() * 257;
+    const green = Math.random() * 257;
+    const blue = Math.random() * 257;
 
-    let x = event.clientX;
-    let y = event.clientY;
+    newAvailableColor.style = `background-color: rgb(${red}, ${green}, ${blue});`
+});
 
-    const red = Math.random() * 256;
-    const green = Math.random() * 256;
-    const blue = Math.random() * 256;
+colors.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    if(event.target.className === 'availableColor') {
+        openModalWindow(event);
+    }
+});
 
-    newCircleElement.style = `background-color: rgb(${red}, ${green}, ${blue});`;
-    newCircleElement.style.left = (x - 22) + 'px';
-    newCircleElement.style.top = (y - 20) + 'px';
+function openModalWindow(event) {
+    const chosenColor = event.target;
+    chosenColor.setAttribute('status', 'current');
+    const x = event.clientX;
+    const y = event.clientY;
 
-    newAvailableColor.style = `background-color: rgb(${red}, ${green}, ${blue});`;
+    const modalWindow = document.createElement('div');
+    modalWindow.classList.add('modal');
+    chosenColor.appendChild(modalWindow);
+    modalWindow.style.top = y + 'px';
+    modalWindow.style.left = x + 'px';
 
-})
+    const form = document.createElement('form');
+    form.classList.add('formStyle');
+    modalWindow.appendChild(form);
+
+    const deleteElement = document.createElement('span');
+    deleteElement.className = 'delete-item';
+    deleteElement.innerHTML = '<i class="fa fa-remove"></i>';
+    form.before(deleteElement);
+
+    const diameter = document.createElement('label');
+    diameter.innerHTML = `Діаметр кола:
+    <input
+        type="number"
+        name="name"
+        id="name"
+        placeholder=""
+        class="diameter-input"
+        required
+        autofocus
+    >`;
+
+  const color = document.createElement('label');
+  color.innerHTML = `Колір кола:
+  <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder=""
+        class="color-input"
+        required
+        autofocus
+    >`;
+
+    form.appendChild(diameter);
+    form.appendChild(color);
+
+    const updateBtn = document.createElement('button');
+    updateBtn.innerHTML = 'Update';
+    form.appendChild(updateBtn);
+
+    const resetBtn = document.createElement('button');
+    resetBtn.innerHTML = 'Cancel';
+    form.appendChild(resetBtn);
+
+    modalWindow.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const diameterValue = document.querySelector('.diameter-input');
+        const colorValue = document.querySelector('.color-input');
+        modalWindow.parentElement.style.backgroundColor = colorValue.value;
+        modalWindow.parentElement.style.width = diameterValue.value + 'px';
+        modalWindow.parentElement.style.height = diameterValue.value + 'px';
+    });
+
+    resetBtn.addEventListener('click', () => {
+        const modalForm = document.querySelector('.formStyle');
+        modalForm.reset();
+    });
+
+    deleteElement.addEventListener('click', () => {
+        const modalWindow = document.querySelector('.modal');
+        const currentCircle = modalWindow.parentElement;
+        currentCircle.removeChild(modalWindow);
+    });
+};
