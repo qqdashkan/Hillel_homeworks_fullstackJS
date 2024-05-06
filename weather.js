c76d9731a7c288532e177a747d4ee1762543268b
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-05-06 15:26:37 +0300

    done 100%

--- weather.js
+++ weather.js
@@ -21,8 +21,6 @@ class Weather {
     };
 
     print(weather) {
-        //Array(20).fill('*').join('');
-        //const arr = [[`${weather.key}`, `${this[key]}`]];
         const arr = Object.entries(weather);
         for (let item of arr) {
             console.log(Array(25).fill('*').join(''));
@@ -44,7 +42,7 @@ weather.print(weather);
 
 class Vehicle {
     start() {
-        console.log('in move');
+        return 'in move';
     };
 
     stop() {
@@ -55,7 +53,7 @@ class Vehicle {
 class Car extends Vehicle {
     constructor() {
         super();
-        }
+    }
 
     startEngine() {
         return 'starting the engine!';
@@ -69,7 +67,7 @@ class Car extends Vehicle {
 class Bike extends Vehicle {
     constructor() {
         super();
-        }
+    }
 
     ringBell() {
         return 'beep beep!'; 
@@ -82,7 +80,14 @@ class Bike extends Vehicle {
 class Truck extends Car {
     constructor() {
         super();
-        }
+    }
+    start() {
+        return 'get into gear';
+    };
+
+    stop() { 
+        return 'throw out of gear';
+    };
 
     startEngine() {
         return 'starting the engine!';
@@ -96,13 +101,166 @@ class Truck extends Car {
 class Sedan extends Car {
     constructor() {
         super();
-        }
-
-    park() {
-        return 'parking the sedan!'; 
+    }
+    start() {
+        return 'get into gear';
+    };
+    stop() { 
+        return 'throw out of gear';
+    };
+        
+    roof() {
+        return 'roof is open!'; 
     };
     radio() {
         return 'music is playing'; 
     }    
 }
 
+class SportBike extends Bike {
+    constructor() {
+        super();
+    }
+    start() {
+        return 'engine is on';
+    };
+    
+    stop() { 
+        return 'engine is off';
+    };           
+    park() {
+            return 'parking the sedan!'; 
+    };
+    someMethod() {
+        return 'doing something'; 
+    }    
+}
+
+class Scooter extends Bike {
+    constructor() {
+        super();
+    }
+    start() {
+        return 'engine is on';
+    };
+
+    stop() { 
+        return 'engine is off';
+    };        
+
+    DVR() {
+        return 'keeps a record!'; 
+    };
+    deleteDVR() {
+        return 'delete video'; 
+    }    
+}
+
+//task 3
+
+let orders = [];
+
+let order1 = {
+    table: 1,
+    dishes: [
+  { name: "Чай", price: 30 },
+  { name: "Еклер", price: 50 }
+    ],
+    status: "в обробці"
+};
+
+let order2 = {
+    table: 2,
+    dishes: [
+  { name: "Матча", price: 45 },
+  { name: "Тірамісу", price: 65 }
+    ],
+    status: "готується"
+};
+
+let order3 = {
+    table: 3,
+    dishes: [
+  { name: "Бургер", price: 40 },
+  { name: "Морозиво", price: 25 },
+  { name: "Американо", price: 15 }
+    ],
+    status: "новий"
+};
+
+class System {
+    changeOrderStatus(order, tableNumber, status) {
+        orders.push(order);
+        let myOrder = orders.find(order => order.table === tableNumber);
+        return myOrder.status = status;
+    };
+
+    cancelOrder(tableNumber) {
+        return orders.filter(order => order.table != tableNumber);
+    };
+}
+
+class Table extends System {
+    constructor() {
+        super();
+    };
+
+    getTotalAmount(tableNumber) {
+        let myOrder = orders.find(order => order.table === tableNumber)
+        let dishes = myOrder.dishes;
+        let sum = dishes.map(dish => dish.price)
+        return sum.reduce((sum, current) => sum + current, 0);
+    };
+
+    addTable(order, table) {
+        return {
+            table: table,
+            dishes: order,
+            status: ""
+        };
+    };
+}
+
+class Order extends Table {
+    dishes = [];
+    constructor() {
+        super();
+    };
+
+    getTotalAmount(order) {
+        let dishes = order.dishes;
+        let sum = dishes.map(dish => dish.price)
+        return sum.reduce((sum, current) => sum + current, 0);
+    };
+
+    addOrder(name, price) {
+        let obj = {
+            name: name,
+            price: price
+        };
+        return this.dishes.push(obj);
+    }
+}
+const system = new System();
+const table = new Table();
+
+const order = new Order();
+const position = order.addOrder('Мак Флурі', 25);
+const position1 = order.addOrder('Big Mac', 45);
+const food = order.dishes;
+
+const order4 = table.addTable(food, 4);
+
+order.getTotalAmount(order4);
+system.changeOrderStatus(order4, 4, 'новий');
+
+const barOrder = new Order();
+const drink1 = barOrder.addOrder('Tea', 15);
+const drinks = barOrder.dishes;
+
+const order5 = table.addTable(drinks, 5);
+
+order.getTotalAmount(order5);
+system.changeOrderStatus(order5, 5, 'новий');
+
+orders = system.cancelOrder(5);
