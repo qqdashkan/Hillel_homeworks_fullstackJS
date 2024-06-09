8b14799c8dab5b426952be4220fd5d54c4ce2be7
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-06-09 16:14:32 +0300

    start

---
+++ react-first-hw-demy/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+export function Header() {
+
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>   
+    );
+}
---
+++ react-first-hw-demy/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Header } from './components/Header';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
+
