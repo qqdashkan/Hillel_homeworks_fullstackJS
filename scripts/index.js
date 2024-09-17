dcfe1988d1d78fc950ed28192e95d301d4114b49
Daria Neshcheretnaya <dashkan1994@gmail.com>
2024-09-17 17:03:34 +0300

    added webpack bundle

--- scripts/index.js
+++ scripts/index.js
@@ -4,41 +4,43 @@ import { ACCESS_KEY } from './constants.js';
 import { fillDetails } from './showElements.js';
 import { fillBackgroundPhoto } from './showElements.js';
 
+const cityWeatherUrl = `${WEATHER_API_DOMAIN}&q=${city}`;
+const cityWallUnspleshUrl = `${UNSPLESH_API_DOMAIN}?query=${city}&client_id=${ACCESS_KEY}&count=1`;
+
 function getWeatherDetails(weatherUrl) {
-    fetch(weatherUrl)
-        .then((data) => data.json())
-        .then(fillDetails)
-        .catch((error) => {
-            console.log(error)
-            console.error(error.message);
-            alert('something went wrong, please return to us in few minutes')
-        })
+  fetch(weatherUrl)
+    .then((data) => data.json())
+    .then(fillDetails)
+    .catch((error) => {
+      console.log(error);
+      console.error(error.message);
+      alert('something went wrong, please return to us in few minutes');
+    });
 }
 
 function getCityWall(unspleshUrl) {
-    fetch(unspleshUrl)
-        .then((data) => data.json())
-        .then(fillBackgroundPhoto)
-        .catch((error) => {
-            console.log(error)
-            console.error(error.message);
-            alert('have no photo')
-        })
+  fetch(unspleshUrl)
+    .then((data) => data.json())
+    .then(fillBackgroundPhoto)
+    .catch((error) => {
+      console.log(error);
+      console.error(error.message);
+      alert('have no photo');
+    });
 }
 
-
 function handleSubmit(event) {
-    event.preventDefault();
-    const city = document.getElementById('city').value.trim();
-    if (city?.length >= 1) {
-        getWeatherDetails(`${WEATHER_API_DOMAIN}&q=${city}`);
-        getCityWall(`${UNSPLESH_API_DOMAIN}?query=${city}&client_id=${ACCESS_KEY}&count=1`);
-    }
+  event.preventDefault();
+  const city = document.getElementById('city').value.trim();
+  if (city?.length >= 1) {
+    getWeatherDetails(cityWeatherUrl);
+    getCityWall(cityWallUnspleshUrl);
+  }
 }
 
 function weatherApp() {
-    const form = document.getElementById('city-form');
-    form.addEventListener('submit', handleSubmit);
+  const form = document.getElementById('city-form');
+  form.addEventListener('submit', handleSubmit);
 }
 
-weatherApp()
+weatherApp();
